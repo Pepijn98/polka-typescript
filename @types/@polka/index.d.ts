dffea42d632cae3f2398ca3e6b018003afce4ac4
KurozeroPB <root@kurozeropb.info>
2019-06-13 10:04:56 +0200

    Add type definitions for @polka/send

--- @types/@polka/index.d.ts
+++ @types/@polka/index.d.ts
@@ -11,6 +11,17 @@ declare module "@polka/send-type" {
     ): void;
 }
 
+declare module "@polka/send" {
+    import { ServerResponse } from "http";
+
+    export default function(
+        res: ServerResponse,
+        code?: number,
+        data?: string | ArrayBuffer | Record<string|number|symbol, any>,
+        headers?: Record<string|number|symbol, any>
+    ): void;
+}
+
 declare module "@polka/url" {
     import { IncomingMessage } from "http";
 
